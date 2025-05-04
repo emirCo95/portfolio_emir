41f461529e5db6f51934b62dd8796a52a52ff4af
Emir Cokrlija <144956911+emirCo95@users.noreply.github.com>
2025-05-04 13:25:25 -0400

    project finish

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,12 @@
 import Image from 'next/image';
 import image from '@/public/image.jpg';
 import project_1 from '@/public/project_1.png';
+import project_2 from '@/public/project_2.png';
+import project_3 from '@/public/project_3.png';
+import project_4 from '@/public/project_4.png';
 import { Exo_2 } from 'next/font/google';
 import { useRouter } from 'next/navigation';
+import { Github, Linkedin } from 'lucide-react';
 
 const exo = Exo_2({
   subsets: ['latin'],
@@ -27,14 +31,78 @@ export default function Home() {
           className="h-full object-fill"
         />
       </div>
-      <div className="">
-        <h1 className="text-center p-6 text-7xl">EMIR COKRLIJA</h1>
+      <div className="relative">
+        <div className="absolute bottom-0 right-0 flex gap-4 p-5">
+          <Linkedin
+            className="cursor-pointer"
+            onClick={() =>
+              router.push(
+                'https://www.linkedin.com/in/emir-cokrlija-28299b1a6/'
+              )
+            }
+          />
+          <Github
+            className="cursor-pointer"
+            onClick={() => router.push('https://github.com/emirCo95')}
+          />
+        </div>
+        <h1 className="text-center p-6 text-7xl bg-black text-white">
+          EMIR COKRLIJA
+        </h1>
         <div className="grid grid-cols-3 grid-rows-2">
           <div
             onClick={() => router.push('https://hiking-landing.netlify.app/')}
-            className="p-5 hover:scale-105 transition ease-in-500 cursor-pointer"
+            className="w-[300px] h-[300px] p-5 hover:scale-105 transition ease-in-500 cursor-pointer"
           >
-            <Image src={project_1} alt="project 1" width={500} height={500} />
+            <Image
+              className="w-full h-full object-cover"
+              src={project_1}
+              alt="project 1"
+              width={500}
+              height={500}
+            />
+          </div>
+          <div
+            onClick={() =>
+              router.push('https://borcelle-boutique.netlify.app/')
+            }
+            className="w-[300px] h-[300px] p-5 hover:scale-105 transition ease-in-500 cursor-pointer"
+          >
+            <Image
+              className="w-full h-full object-cover"
+              src={project_2}
+              alt="project 2"
+              width={500}
+              height={500}
+            />
+          </div>
+          <div
+            onClick={() =>
+              router.push('https://cuisine-restaurant1.netlify.app/')
+            }
+            className="w-[300px] h-[300px] p-5 hover:scale-105 transition ease-in-500 cursor-pointer"
+          >
+            <Image
+              className="w-full h-full object-cover border-2 border-black"
+              src={project_3}
+              alt="project 3"
+              width={500}
+              height={500}
+            />
+          </div>
+          <div
+            onClick={() =>
+              router.push('https://elysium-photography.netlify.app/')
+            }
+            className="w-[300px] h-[300px] p-5 hover:scale-105 transition ease-in-500 cursor-pointer"
+          >
+            <Image
+              className="w-full h-full object-cover border-2 border-black"
+              src={project_4}
+              alt="project 4"
+              width={500}
+              height={500}
+            />
           </div>
         </div>
       </div>
