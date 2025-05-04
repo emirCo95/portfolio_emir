b3fd1ec2e45dd6032151dabcf292589376f51bc0
Emir Cokrlija <144956911+emirCo95@users.noreply.github.com>
2025-05-04 14:06:00 -0400

    refactor

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import type { Metadata } from 'next';
 import './globals.css';
 
 export const metadata: Metadata = {
-  title: 'Emir Cokrlijja',
+  title: 'Emir Cokrlija',
   description: 'Portfolio',
 };
 
